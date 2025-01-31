2da60a87a72591e2eb1c69a4f98704050e2ee566
Cristhian Valverde <crisval542@gmail.com>
2025-01-30 22:24:08 -0500

    4commit

--- ProyectoFinal2/ProyectoFinal2/settings.py
+++ ProyectoFinal2/ProyectoFinal2/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
 
+pymysql.install_as_MySQLdb()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,13 +41,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'util.apps.UtilConfig',
+
+
     'pedidos.apps.PedidosConfig',
     'facturacion.apps.FacturacionConfig',
     'mesas.apps.MesasConfig',
     'menus.apps.MenusConfig',
     'estadisticas.apps.EstadisticasConfig',
-    'inventario.apps.InventarioConfig'
+    'inventario.apps.InventarioConfig',
+    'util.apps.UtilConfig',
 ]
 
 MIDDLEWARE = [
@@ -85,8 +89,15 @@ WSGI_APPLICATION = 'ProyectoFinal2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'k8l9@_34P',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
